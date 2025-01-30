8ecb15857bb7ac0443e96ecc456f3d761dfac72b
Lawwee <lawalmohammed567@gmail.com>
2025-01-30 21:41:04 +0100

    created new file for email reply model

---
+++ src/models/emailReply.ts
@@ -0,0 +1,100 @@
+import { model, Schema } from "mongoose";
+
+const replySchema = new Schema({
+    agentId: {
+        type: String,
+        required: [true, "Agent ID is required"]
+    },
+    client: {
+        type: Schema.Types.ObjectId,
+        required: [true, "Client is required"],
+        ref: "User"
+    },
+    campaignName: {
+        type: String
+    },
+    campaignId: {
+        type: Number
+    },
+    clientId: {
+        type: Number
+    },
+    webhookId: {
+        type: Number
+    },
+    webhookName: {
+        type: String
+    },
+    campaignStatus: {
+        type: String
+    },
+    statsId: {
+        type: String
+    },
+    fromEmail: {
+        type: String,
+        required: [true, "From email is required"]
+    },
+    subject: {
+        type: String
+    },
+    sentMessage: {
+        messageId: String,
+        html: String,
+        text: String,
+        time: String
+    },
+    toEmail: {
+        type: String,
+        required: [true, "To email is required"]
+    },
+    toName: {
+        type: String
+    },
+    eventTimestamp: {
+        type: String
+    },
+    replyMessage: {
+        messageId: String,
+        html: String,
+        text: String,
+        time: String
+    },
+    sequenceNumber: {
+        type: Number
+    },
+    secretKey: {
+        type: String
+    },
+    appUrl: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    metadata: {
+        webhookCreatedAt: String
+    },
+    webhookUrl: {
+        type: String
+    },
+    eventType: {
+        type: String
+    },
+    slEmailLeadId: {
+        type: Number
+    },
+    slEmailLeadMapId: {
+        type: Number
+    },
+    slLeadEmail: {
+        type: String
+    },
+    repliedTo: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, { timestamps: true });
+
+export const ReplyModel = model("EmailReply", replySchema);
