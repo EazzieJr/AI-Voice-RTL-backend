afa4fda625afbcf8b8d2dc29182147e2f1958ece
Lawwee <lawalmohammed567@gmail.com>
2025-03-02 23:18:50 +0100

    updated call_started and call_ended events

--- src/services/call.ts
+++ src/services/call.ts
@@ -6,8 +6,6 @@ import { reviewCallback, reviewTranscript } from "../utils/transcript-review";
 import callHistoryModel from "../models/historyModel";
 import { DailyStatsModel } from "../models/logModel";
 import { updateStatsByHour } from "../controllers/graphController";
-import { time } from "console";
-import { nextDay } from "date-fns";
 import axios from "axios";
 import { AuthRequest } from "../middleware/authRequest";
 import { CancelScheduleSchema, ScheduleCallSchema } from "../validations/call";
@@ -15,7 +13,6 @@ import moment from "moment-timezone";
 import { scheduleCronJob } from "../utils/scheduleJob";
 import schedule from "node-schedule";
 import { DateTime } from "luxon";
-import { userModel } from "../models/userModel";
 
 class CallService extends RootService {
 
@@ -145,7 +142,7 @@ class CallService extends RootService {
             try {
                 if (event === "call_started") {
                     console.log(`call started for: ${data.call_id}`);
-                    await this.call_started(payload, next);
+                    await this.call_started(payload, res, next);
                 } else if (event === "call_ended") {
                     console.log(`call ended: ${data.call_id}`);
                     await this.call_ended(payload, todayString, todayStringWithTime, time, next);
@@ -172,40 +169,24 @@ class CallService extends RootService {
         };
     };
 
-    async call_started(payload: any, next: NextFunction) {
+    async call_started(payload: any, res: Response, next: NextFunction) {
         try {
             const { event, data } = payload;
             console.log("data: ", payload);
 
             if (event === "call_started") {
                 const { call_id, agent_id } = data;
-
-                await contactModel.findOneAndUpdate(
+                
+                await contactModel.updateOne(
                     { callId: call_id, agentId: agent_id },
-                    { dial_status: callstatusenum.IN_PROGRESS }
+                    { $set: { dial_status: callstatusenum.IN_PROGRESS } }
                 );
 
-                const new_event = await EventModel.create({
-                    callId: call_id,
-                    agentId: agent_id
-                });
-
-                if (!new_event._id) {
-                    console.log("Error creating new event model");
-                };
-
-                const hist_data = await callHistoryModel.create({
-                    callId: call_id,
-                    agentId: agent_id
-                });
-
-                if (!hist_data._id) {
-                    console.log("Error creating new event model");
-                };
+                console.log("call started for: ", call_id);
 
             } else {
                 console.error("Event must be call_started: ", event);
-            }            
+            };    
 
         } catch (e) {
             console.error("Unable to get data from started call: " + e);
@@ -218,21 +199,22 @@ class CallService extends RootService {
             console.log("payload: ", payload);
             const { event, call, data } = payload;
             const {
+                call_type,
                 call_id,
-                agent_id,
-                call_status,
                 start_timestamp,
                 end_timestamp,
                 transcript,
                 disconnection_reason,
                 recording_url,
                 public_log_url,
-                call_analysis,
                 retell_llm_dynamic_variables,
                 from_number,
                 to_number,
-                direction
+                direction,
+                call_status
             } = data;
+
+            const { agent_id, duration_ms } = call;
 
             let analyzedTranscriptForStatus;
             let callStatus;
@@ -261,12 +243,12 @@ class CallService extends RootService {
 
                 const call_back_date = await reviewCallback(transcript);
 
-                const duration_in_HMS = convertMsToHourMinSec(call.duration_ms);
+                const duration_in_HMS = convertMsToHourMinSec(duration_ms);
                 const total_duration = convertMsToHourMinSec(end_timestamp - start_timestamp) || 0;
 
                 const call_ended_updated_data = {
                     callId: call_id,
-                    agentId: call.agent_id,
+                    agentId: agent_id,
                     recordingUrl: recording_url,
                     callDuration: duration_in_HMS,
                     disconnectionReason: disconnection_reason,
@@ -277,14 +259,10 @@ class CallService extends RootService {
                     ...(transcript && { transcript }),
                 };
 
-                const ended_data_update = await EventModel.findOneAndUpdate(
-                    { callId: call_id, agentId: call.agent_id },
-                    { $set: call_ended_updated_data },
-                    { returnOriginal: false }
-                );
+                const transcript_data = await EventModel.create(call_ended_updated_data);
 
                 statsUpdate.$inc.totalCalls = 1;
-                statsUpdate.$inc.totalCallDuration = call.duration_ms;
+                statsUpdate.$inc.totalCallDuration = duration_ms;
         
                 if (is_machine) {
                     statsUpdate.$inc.totalAnsweredByVm = 1;
@@ -312,8 +290,6 @@ class CallService extends RootService {
                     callStatus = callstatusenum.CALLED;
                 };
 
-                // console.log("dial: ", callStatus);
-
                 const callData = {
                     callId: call_id,
                     agentId: agent_id,
@@ -331,9 +307,9 @@ class CallService extends RootService {
                     transcriptWithToolCalls:
                         payload.data.transcript_with_tool_calls || [],
                     publicLogUrl: public_log_url || null,
-                    callType: data.call_type || null,
-                    customAnalysisData:
-                        event === "call_analyzed" ? call_analysis : null,
+                    callType: call_type || null,
+                    // customAnalysisData:
+                    //     event === "call_analyzed" ? call_analysis : null,
                     fromNumber: from_number || null,
                     toNumber: to_number || null,
                     direction: direction || null,
@@ -342,59 +318,44 @@ class CallService extends RootService {
                     dial_status: callStatus,
                 };
 
-                // console.log("callData: ", callData);
-
-                const history_update = await callHistoryModel.findOneAndUpdate(
-                    { callId: call_id, agentId: agent_id },
-                    { $set: callData },
-                    { returnOriginal: false }
-                );
-
-                // console.log("resu: ", history_update);
+                await callHistoryModel.create(callData);
 
                 const jobId_from_retell = retell_llm_dynamic_variables.job_id ? retell_llm_dynamic_variables.job_id : null;
 
                 let statResults;
 
-                console.log("today: ", todayString);
-                console.log("jobId: ", jobId_from_retell);
-
                 statResults = await DailyStatsModel.findOneAndUpdate(
                     { day: todayString, agentId: agent_id, jobProcessedBy: jobId_from_retell },
-                    { $set: statsUpdate },
+                    statsUpdate,
                     { returnOriginal: false }
                 );
-
-                console.log("update: ", statResults);
 
                 const timestamp = new Date();
 
                 await updateStatsByHour(agent_id, todayString, timestamp);
 
-
-                // console.log("ended event: ", ended_data_update);
                 const updateData: any = {
                     dial_status: callStatus,
                     $push: {
                         datesCalled: todaysDateForDatesCalled
                     },
-                    referenceToCallId: ended_data_update._id,
+                    referenceToCallId: transcript_data._id,
                     timesCalled: time,
                     $inc: { calledTimes: 1 }
                 };
 
                 if (statResults) {
-                    updateData.linkToCallLogModel = statResults._id;
+                    updateData.linktocallLogModel = statResults._id;
                 } else {
                     console.log("stat: ", statResults);
                 };
 
-                const update_contact = await contactModel.findOneAndUpdate(
+                await contactModel.findOneAndUpdate(
                     { callId: call_id, agentId: agent_id },
                     { $set: updateData }
                 );
 
-                // console.log("updat: ", update_contact);
+                console.log("call ended for: ", call_id);
                 
             } else {
                 console.error("Event must be call_ended: ", event);
